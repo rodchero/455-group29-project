4cbc340c44ab44c2669e4c5b13ba1be04e881509
jkkiveko <jameskk2004@gmail.com>
2026-03-20 16:56:25 -0400

    Rework main.py to use the EA class

--- main.py
+++ main.py
@@ -2,20 +2,31 @@ from city import *
 from individual import *
 from ea import *
 
+POP_SIZE = 2
+NUM_GENERATIONS = 1000
+MUTATION_RATE = 0.1
 
 def main():
 
+    # urban_blueprint = {
+    #     'school': 2,
+    #     'emergency': 3,
+    #     'grocery': 4
+    # }
+
     urban_blueprint = {
-        'school': 2,
-        'emergency': 3,
-        'grocery': 4
+        'school': 1,
+        'emergency': 2,
     }
     
-    # population generation
-    population = [Individual(urban_blueprint) for _ in range(10)]
+    ea = EA(POP_SIZE, NUM_GENERATIONS, MUTATION_RATE, urban_blueprint, City(seed=42))
+
+    print(City(seed=42))
 
     print("Initial Population:")
-    print(population)
+    print(ea.population)
+
+    ea.visualize_population(2)
     
 
 if __name__ == "__main__":
